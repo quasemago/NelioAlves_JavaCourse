e3cd067cf15bed4e5a546ba8964afb1b69ddbaf0
quasemago <brunoronningfn@gmail.com>
2023-12-15 17:18:08 -0400

    Added the solution to Introduction to OOP exercise 1

--- Introdução_POO/Exercise1/src/main/java/application/Program.java
+++ Introdução_POO/Exercise1/src/main/java/application/Program.java
@@ -14,8 +14,24 @@ Utilize a seguinte classe:
 + Diagonal() : double
  */
 
+import entities.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        final Scanner sc = new Scanner(System.in);
 
+        System.out.println("Informe largura e altura do retângulo, respectivamente.");
+        double largura = sc.nextDouble();
+        double altura = sc.nextDouble();
+
+        Rectangle rec = new Rectangle(largura, altura);
+
+        System.out.printf("Área: %.2f%n", rec.area());
+        System.out.printf("Perímetro: %.2f%n", rec.perimeter());
+        System.out.printf("Diagonal: %.2f%n", rec.diagonal());
     }
 }
