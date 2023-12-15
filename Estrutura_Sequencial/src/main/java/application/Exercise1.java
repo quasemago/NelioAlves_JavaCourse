c871683b3bb423a01ea229830941db03b1b53634
quasemago <brunoronningfn@gmail.com>
2023-12-15 14:31:18 -0400

    Added the solution to exercise 2 and rename exercise 1 class

--- Estrutura_Sequencial/src/main/java/application/Exercise1.java
+++ Estrutura_Sequencial/src/main/java/application/Exercise1.java
@@ -7,7 +7,7 @@ package application;
 
 import java.util.Scanner;
 
-public class Exercicio1 {
+public class Exercise1 {
         public static void main(String[] args) {
             final Scanner sc = new Scanner(System.in);
 
